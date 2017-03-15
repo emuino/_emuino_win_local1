57351687611ce3b8766081ca3286359168ddc4db
gymadarasz <gyula.madarasz@gmail.com>
2017-03-15 17:37:11 +0000

    basic web drag drop UI

--- miniweb/htdocs/emuino/emuino.js
+++ miniweb/htdocs/emuino/emuino.js
@@ -1,15 +1,75 @@
+// TODO: add device extensions to a separated namespace, dont take it to main window object
 
-		console.log('Emuino client started');
-		
-		var ws = new WebSocket('ws://127.0.0.1:8080/');
+// device extensions
+var Arduino = function($elem, guid, args) {
+	
+	console.log('init an arduino - $elem, guid, args: ', $elem, guid, args);
+	$elem.html('An Arduino info here..');
+	
+	// devices loops
+	setInterval(function(){
+		for(var k in devices) {
+			console.log('arduino device is working in loop..');
+		};
+	}, 400);
+};
 
-		ws.onmessage = function(event) {
-			console.log(event);
-			document.getElementById('msgBox').innerHTML = event.data;
-			document.getElementById('outMsg').value='';
-		}
+// TODO: add more extension here or load dynamically
 
-		function send()
-		{
-			ws.send(document.getElementById('outMsg').value);
-		}		
+
+// TODO add it into a namespace or object or something but dont take in into the main wondow object
+
+	var devices = [];
+	
+	
+	
+	var dndCounter = 0;
+	var dndZIndexMax = 0;
+	var make = function(name, guid, args) {
+		if(!args) args = {};
+		dndCounter++;
+		var dndNextID = 'dnd-'+dndCounter+'-'+name+'-'+guid;
+		$('.dnd-container').append(
+			'<div id="'+dndNextID+'" class="dnd-box">'+
+			'	<div class="dnd-title">'+name+' (guid:'+guid+')</div>'+
+			'	<div id="'+dndNextID+'-contents" class="dnd-contents"></div>'+
+			'</div>'
+		);		
+		$('#'+dndNextID).draggable({
+			handle: '.dnd-title'
+		});
+		$('#'+dndNextID+' .dnd-title').disableSelection();
+		$('#'+dndNextID).resizable();
+		$('#'+dndNextID).css({
+			top: ((dndCounter%10)*30) + 'px',
+			left: ((dndCounter%10)*30) + 'px',
+			'z-index': (++dndZIndexMax)
+		});
+		$('#'+dndNextID).mousedown(function(){			
+			$(this).css('z-index', (++dndZIndexMax));
+		});
+		var device = new window[name]($('#'+dndNextID+'-contents'), guid, args);
+		devices.push(device);
+	};
+	
+
+	console.log('Emuino client started');
+
+	var ws = new WebSocket('ws://127.0.0.1:8080/');
+
+	ws.onmessage = function(event) {
+		console.log(event);
+		//document.getElementById('msgBox').innerHTML = event.data;
+		//document.getElementById('outMsg').value='';
+		eval(event.data);
+	}
+
+	function send()
+	{
+		// todo...
+		ws.send(document.getElementById('outMsg').value);
+	}	
+
+$(function(){	
+	
+});

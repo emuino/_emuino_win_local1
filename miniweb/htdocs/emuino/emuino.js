d0f350e05b1e03bd74f3f5ff27f245d843edd81d
gymadarasz <gyula.madarasz@gmail.com>
2017-03-15 01:18:05 +0000

    emuino client debug

--- miniweb/htdocs/emuino/emuino.js
+++ miniweb/htdocs/emuino/emuino.js
@@ -4,6 +4,7 @@
 		var ws = new WebSocket('ws://127.0.0.1:8080/');
 
 		ws.onmessage = function(event) {
+			console.log(event);
 			document.getElementById('msgBox').innerHTML = event.data;
 			document.getElementById('outMsg').value='';
 		}

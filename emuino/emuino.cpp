bfc3f8d5dd66a4ee3c5b6bb40dbacfb7231c890d
gymadarasz <gyula.madarasz@gmail.com>
2017-03-15 03:27:54 +0000

    basic communication

--- emuino/emuino.cpp
+++ emuino/emuino.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
 
 // TODO: add it to the wiki
@@ -6,12 +8,64 @@
 class ArduinoSkatch {
 private:
 #include SKATCH
+
+	
+	void fappendln(char* fname, char* msg) {
+		FILE* f = fopen(fname, "a");
+		fprintf(f, "%s\n", msg);
+		fclose(f);		
+	}
+
+	void log(char* msg) {
+		printf("%s\n", msg);		
+		fappendln("emuino.log", msg);
+	}
+	
+	void pipeCliCmd(char* cmd) {
+		log("[pipe cli cmd]: run:");
+		log(cmd);
+		// todo: run client command..
+		
+	}
+	
+	void pipeSend(char* msg) {		
+		log("[pipe send]: start:");
+		log(msg);
+		fappendln("../pipe_srv", msg);
+		log("[pipe send]: finish");
+	}
+	
+	void pipeRead() {	
+		//log("[pipe read]: start");	
+		FILE* f = fopen("../pipe_cli", "r+");
+		if(f!=NULL) {
+			char line[255];
+			while (fgets(line, sizeof(line), f)) {
+				pipeCliCmd(line);
+			}
+			fclose(f);		
+			fclose(fopen("../pipe_cli", "w"));
+			//log("[pipe read]: finish");
+		}
+	}
+	
+	void reset() {
+		log("[arduino]: reset..");
+		// todo: reset pins and all of emulated arduino
+	}
+	
 public:
-	ArduinoSkatch() {		
+	ArduinoSkatch() {
+		log("[EMUINO] start");
+		pipeSend("arduino.reset");
+		reset();
+		log("[arduino sketch]: setup..");
 		setup();
+		log("[arduino sketch]: loop start..");
 		while(!kbhit()) {
+			pipeRead();
 			loop();
-		} 
+		}
 	}
 } skatch;
 
--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 @echo EMUINO - emulation and debugging environment
 @pause
-del pipe_log
-del pipe_lock
+del pipe_*.
 start websocketd\websocketd-0.2.11-windows_386\websocketd.exe --port=8080 wssrv/wssrv.exe
 start miniweb\miniweb.exe
 start "" http://localhost:8000/emuino/emuino.html

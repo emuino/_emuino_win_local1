c605feb513f0efb4a24732fbc7b668ea15d9cfca
gymadarasz <gyula.madarasz@gmail.com>
2017-03-23 21:19:45 +0000

    typo fix runbat help

--- www/runbat.php
+++ www/runbat.php
@@ -25,7 +25,7 @@ if($f = isset($_GET['f']) ? $_GET['f'] : '') {
 } else {
 	die("Batch filename didn't specified, use in request: 
 
-exec.php?f=batchfile.bat&min=1&&bg=1 
+runbat.php?f=batchfile.bat&min=1&&bg=1 
 
 where 'min' and 'bg' arguments are optional 
 if 'min' set and true the opened window is minimized");
